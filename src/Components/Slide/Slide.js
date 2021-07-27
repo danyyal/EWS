300bb6bd89f667a85a1c1b2790a2b5810cde0a44
danyyal <danyyal.ali.31@gmail.com>
2021-07-28 03:26:24 +0500

    changed the view of the slider

--- src/Components/Slide/Slide.js
+++ src/Components/Slide/Slide.js
@@ -2,8 +2,9 @@ import React,{useEffect} from "react";
 import { Carousel } from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Slide.css';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import  {useSelector,useDispatch} from 'react-redux';
+import { Button } from '@material-ui/core';
 import {fetchProductStart} from '../../Redux/Products/Products.actions';
 
 const mapState =({productsData})=>({
@@ -44,34 +45,50 @@ useEffect(() => {
                 }
                 </Carousel>
                   */}
-    <Carousel autoPlay={true} dynamicHeight={true} infiniteLoop={true} interval={2000} >
+    <Carousel autoPlay={true} dynamicHeight={true} infiniteLoop={true} interval={3000} >
     
-      <div>
+      <div className="flexingslider">
+        <div className='sliderText'>
+          <div className="sliderTextInner">
+            <h2>Beautify the World</h2>
+            <p>All Kinds of makeup accessories available</p>
+            <Button><Link to='/Checkout' className='btn'>Get Now</Link></Button>
+          </div>
+        </div>
         <img alt="" src="/images/slide-1.jpg" />
-        <div className='legend'>
-          <h1>Modern Watch</h1>
-          <p>A modern watch that can be used in varoius events or weddings</p>
-          <Link to='/Checkout' className='btn'>BUY NOW</Link>
-        </div>
-
       </div>
-      <div>
+
+      <div className="flexingslider">
+        <div className='sliderText'>
+          <div className="sliderTextInner">
+            <h2>Electronics</h2>
+            <p>Electronics and Accessories</p>
+            <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button>
+          </div>
+        </div>
         <img alt="" src="/images/slide-2.jpg" />
-        <div className='legend'>
-          <h1>WODDEN CHAIR</h1>
-          <p>A COMFORTABLE AND EASY TO SIT</p>
-          <Link to='/cart-page' className='btn'>BUY NOW</Link>
-        </div>
-
       </div>
-      <div>
-        <img alt="" src="/images/slide-3.jpg" />
-        <div className='legend'>
-          <h1>THIRD SLIDE</h1>
-          <p>BDFBWF KSNFNQEQHFUH BFEFEHWB BWEHHW</p>
-          <Link to='/cart-page' className='btn'>BUY NOW</Link>
-        </div>
 
+      <div className="flexingslider">
+        <div className="sliderText">
+          <div className="sliderTextInner">  
+            <h2>Clothing</h2>
+            <p className="sliderTextDesc">All kinds of Clothes available</p>
+            <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button>
+          </div>
+        </div>
+        <img alt="" src="/images/slide-3.jpg" />
+      </div>
+
+      <div className="flexingslider">
+        <div className="sliderText">
+          <div className="sliderTextInner">
+            <h2>Get perfection</h2>
+            <p className="sliderTextDesc">Different types of Vehicles</p>
+            <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button>
+          </div>
+        </div>
+        <img alt="" src="/images/slide-4.jpg" />
       </div>
     </Carousel>
     </>
