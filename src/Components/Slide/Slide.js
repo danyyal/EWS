06367d8356a07bef696a433f57754b6f65042029
danyyal <danyyal.ali.31@gmail.com>
2021-09-05 19:44:11 +0500

    Added the carousel slider

--- src/Components/Slide/Slide.js
+++ src/Components/Slide/Slide.js
@@ -1,96 +1,94 @@
-import React,{useEffect} from "react";
+import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Slide.css';
-import { Link} from 'react-router-dom';
-import  {useSelector,useDispatch} from 'react-redux';
-import { Button } from '@material-ui/core';
-import {fetchProductStart} from '../../Redux/Products/Products.actions';
 
-const mapState =({productsData})=>({
-  products:productsData.products
-})
-
-const Slide=() =>{
-  const dispatch = useDispatch();
-const {products} = useSelector(mapState);
-const {data} = products;
-useEffect(() => {
-  dispatch(fetchProductStart());
-}, [])
-
+const Slide = () => {
   return (
     <>
-     {/* <Carousel autoPlay={true} dynamicHeight={true} infiniteLoop={true} interval={2000} >
-          { data && data.length>0 ? data.map((product, index) => {
-                    const { productThumbnail, productName, productPrice, productDesc } = product;
-                    if (!productThumbnail || !productName || typeof productPrice === 'undefined') return null;
-                    return (
-                     
-                      <div>
-                        <img alt="" src={productThumbnail} />
-                        <div className='legend'>
-                          <h1>{productName}</h1>
-                          <p>{productDesc}</p>
-                          <h5>Price:{productPrice}</h5>
-                          <Link to='/Checkout' className='btn'>BUY NOW</Link>
-                        </div>
-                
-                      </div>
-                
-                    
-                    );
-                }) : 'Loading...'
-                
-                }
-                </Carousel>
-                  */}
-    <Carousel autoPlay={true} dynamicHeight={true} infiniteLoop={true} interval={3000} >
-    
-      <div className="flexingslider">
-        <div className='sliderText'>
-          <div className="sliderTextInner">
-            <h2>Beautify the World</h2>
-            <p>All Kinds of makeup accessories available</p>
-            {/* <Button><Link to='/Checkout' className='btn'>Get Now</Link></Button> */}
+      <Carousel autoPlay={true} dynamicHeight={true} infiniteLoop={true} interval={3000} >
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>Beautify the World</h2>
+              <p>All garden accessories are Available </p>
+              <Button><Link to='/Products/homeandgarden' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/garden.jpg" />
           </div>
         </div>
-        <img alt="" src="/images/slide-1.jpg" />
-      </div>
 
-      <div className="flexingslider">
-        <div className='sliderText'>
-          <div className="sliderTextInner">
-            <h2>Electronics</h2>
-            <p>Electronics and Accessories</p>
-            {/* <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button> */}
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>Electroics Accessories</h2>
+              <p>Welcome Auto lovers Shop With Confidence</p>
+              <Button><Link to='/Products/phonesandcommunicationdevices' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/electronic.jpg" />
           </div>
         </div>
-        <img alt="" src="/images/slide-2.jpg" />
-      </div>
 
-      <div className="flexingslider">
-        <div className="sliderText">
-          <div className="sliderTextInner">  
-            <h2>Clothing</h2>
-            <p className="sliderTextDesc">All kinds of Clothes available</p>
-            {/* <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button> */}
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>Revamp Your Wardrob</h2>
+              <p>Beautiful Branded Dresses with best Fabric is Here</p>
+              <Button><Link to='/Products/womencloth' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/dress.jpg" />
           </div>
         </div>
-        <img alt="" src="/images/slide-3.jpg" />
-      </div>
 
-      <div className="flexingslider">
-        <div className="sliderText">
-          <div className="sliderTextInner">
-            <h2>Get perfection</h2>
-            <p className="sliderTextDesc">Different types of Vehicles</p>
-            {/* <Button><Link to='/cart-page' className='btn'>Get Now</Link></Button> */}
+
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>Decore Your Home</h2>
+              <p>All accessories required to decore home are Available</p>
+              <Button><Link to='/Products/homeimprovements' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/decor.jpg" />
           </div>
         </div>
-        <img alt="" src="/images/slide-4.jpg" />
-      </div>
-    </Carousel>
+
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>Sports & Entertainment</h2>
+              <p>All Sports and Fun Game Accessories are Here</p>
+              <Button><Link to='/Products/sportsandentertainment' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/sport.jpg" />
+          </div>
+        </div>
+
+
+        <div className="flexingslider">
+          <div className='sliderText'>
+            <div className="sliderTextInner">
+              <h2>AutoMobile and Motor Cycle </h2>
+              <p>Welcome Auto Lovers shop with confedence</p>
+              <Button><Link to='/Products/automobilesandmotorcycle' className='btn'>View Now</Link></Button>
+            </div>
+          </div>
+          <div className='sliderImg'>
+            <img alt="" src="/images/car.jpg" />
+          </div>
+        </div>
+      </Carousel>
     </>
   );
 }
